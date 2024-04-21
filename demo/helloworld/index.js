b62681e4e63c2051ee48c7b33e39b0db527e5a26
ppzreboot <164493036+ppzreboot@users.noreply.github.com>
2024-04-21 06:23:53 +0000

    perf: hello

--- demo/helloworld/index.js
+++ demo/helloworld/index.js
@@ -1,9 +1,10 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log('收到请求', new Date())
-  res.write('Hello, Node.js')
-  res.end()
+  // 这里的代码，在“服务器收到 http 请求”后执行
+  console.log('收到请求', new Date()) // 打印日志
+  res.write('Hello, Node.js') // 回信内容（响应内容）
+  res.end() // 回信完毕
 })
 
 server.listen(8080)
