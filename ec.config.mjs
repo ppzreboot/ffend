1c7a82b34ae32c70908d1822fe28817acf469e1c
ppzreboot <164493036+ppzreboot@users.noreply.github.com>
2024-04-25 03:07:28 +0000

    feat: 添加折叠代码

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,7 +1,11 @@
 import { defineEcConfig } from 'astro-expressive-code'
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
 
 export default
 defineEcConfig({
-  plugins: [pluginLineNumbers()],
+  plugins: [
+    pluginLineNumbers(),
+    pluginCollapsibleSections(),
+  ],
 })
